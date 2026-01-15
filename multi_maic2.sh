85d38c5066ea9da5831e0c22c7ab5b6e7b0688d2
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2026-01-15 11:32:01 +0900

    Script 'multi_maic2.sh' updated.

--- multi_maic2.sh
+++ multi_maic2.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 # (Selection of shell)
 
+################################################################################
+#
+#  m u l t i _ m a i c 2 . s h
+#
+#  bash script for multiple execution of maic2.sh
+#  (compilation, linking and execution of the program MAIC-2).
+#
+#  Author: Ralf Greve
+#
+#  Date: 2026-01-15
+#
+################################################################################
+
 # Simulation #2 by Greve et al. (2010),
 # only over 1 Martian year with more detailed output:
-(./maic2.sh run_c01a) >tmp/out_mjob_01.dat 2>&1
+(./maic2.sh -m run_c01a -f) >tmp/out_mjob_c01a.dat 2>&1
 
 # Simulation #2 by Greve et al. (2010):
-(./maic2.sh run_c01) >tmp/out_mjob_02.dat 2>&1
+(./maic2.sh -m run_c01 -f) >tmp/out_mjob_c01.dat 2>&1
 
 # Simulation #1 by Greve et al. (2010):
-(./maic2.sh run_c02) >tmp/out_mjob_03.dat 2>&1
+(./maic2.sh -m run_c02 -f) >tmp/out_mjob_c02.dat 2>&1
 
 # Simulation #3 by Greve et al. (2010):
-(./maic2.sh run_c03) >tmp/out_mjob_04.dat 2>&1
+(./maic2.sh -m run_c03 -f) >tmp/out_mjob_c03.dat 2>&1
 
 # Simulation #4 by Greve et al. (2010):
-(./maic2.sh run_c04) >tmp/out_mjob_05.dat 2>&1
+(./maic2.sh -m run_c04 -f) >tmp/out_mjob_c04.dat 2>&1
 
 # Simulation #6 by Greve et al. (2010):
-(./maic2.sh run_t06) >tmp/out_mjob_06.dat 2>&1
+(./maic2.sh -m run_t06 -f) >tmp/out_mjob_t06.dat 2>&1
 
 # Simulation #7 by Greve et al. (2010):
-(./maic2.sh run_t07) >tmp/out_mjob_07.dat 2>&1
+(./maic2.sh -m run_t07 -f) >tmp/out_mjob_t07.dat 2>&1
 
 # Simulation #8 by Greve et al. (2010):
-(./maic2.sh run_t08) >tmp/out_mjob_08.dat 2>&1
+(./maic2.sh -m run_t08 -f) >tmp/out_mjob_t08.dat 2>&1
 
 # Simulation #5 by Greve et al. (2010):
-(./maic2.sh run_t12) >tmp/out_mjob_09.dat 2>&1
+(./maic2.sh -m run_t12 -f) >tmp/out_mjob_t12.dat 2>&1
 
 # Simulation #6 by Greve et al. (2010),
 # but from 20 million years ago until 10 million years into the future:
-(./maic2.sh run_t14) >tmp/out_mjob_10.dat 2>&1
+(./maic2.sh -m run_t14 -f) >tmp/out_mjob_t14.dat 2>&1
 
 # Simulation run_t39 by Greve et al. (2012), pp. 14-15:
-(./maic2.sh run_t39) >tmp/out_mjob_11.dat 2>&1
+(./maic2.sh -m run_t39 -f) >tmp/out_mjob_t39.dat 2>&1
 
 # Simulation run_t40 by Greve et al. (2012), pp. 14-15:
-(./maic2.sh run_t40) >tmp/out_mjob_12.dat 2>&1
+(./maic2.sh -m run_t40 -f) >tmp/out_mjob_t40.dat 2>&1
+
+################################################################################
 
 # References:
 #
@@ -51,4 +66,6 @@
 # Glaciation of Mars from 10 million years ago until 10 million years
 # into the future simulated with the model MAIC-2.
 # Presentation No. PPS03-06, JpGU Meeting, Makuhari, Chiba, Japan,
-# 24 May 2012, doi: 10.5281/zenodo.3698542.
+# 24 May 2012, doi: 10.5281/zenodo.3698541.
+
+######################### End of multi_maic2.sh ################################
